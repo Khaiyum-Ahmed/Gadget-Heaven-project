3753ebe059d2cc4fd2c3ecb55d33740fc5e8a43c
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-08-12 12:46:56 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Home from './Components/Home/Home'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-3xl text-center font-bold'>Vite + React</h1>
+    <Home></Home>
     </>
   )
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar";
+
+const Home = () => {
+    return (
+        <div className="max-w-11/12 mx-auto">
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Home;
