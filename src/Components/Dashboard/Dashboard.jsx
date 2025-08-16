39d94e3bfeca7ab65d7071e39d837a7bc5b78b5e
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-08-16 17:23:01 +0600

    sort item done

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -9,9 +9,11 @@ const Dashboard = () => {
 
     const [cartList, setCartList] = useState([]);
 
-    const [wishList, setWishList]= useState([]);
+    const [wishList, setWishList] = useState([]);
 
     const [view, setView] = useState("cart");
+
+    const [sort, setSort] = useState('');
 
     useEffect(() => {
         const storedCartList = getStoredCartList();
@@ -29,13 +31,30 @@ const Dashboard = () => {
         setWishList(addWishList)
     }, [allProducts]);
 
-    
     const totalPrice = (list) => {
-    return list.reduce((sum, item) => sum + item.price, 0);
-  };
+        let sum = 0;
+
+        list.forEach(item => {
+            sum += item.price;
+        });
+
+        return sum;
+    };
+
+    const handleSort = sortPrice =>{
+        setSort(sortPrice);
+        // if(sortPrice === "price"){
+        //     const sortedCartList = [...cartList].sort((a,b)=> b.price - a.price);
+        //     setCartList(sortedCartList)
+        // }
+          const sortedCartList = [...cartList].sort((a,b)=> b.price - a.price);
+          const sortedWishtList = [...wishList].sort((a,b)=> b.price - a.price);
+            setCartList(sortedCartList)
+            setWishList(sortedWishtList)
+    }
 
     const displayedList = view === "cart" ? cartList : wishList;
-    
+
     // console.log(allProducts, cartList)
 
     return (
@@ -48,9 +67,9 @@ const Dashboard = () => {
 
                 <div className="space-x-4 text-[18px]">
 
-                    <button onClick={()=> setView("cart")} className={`rounded-4xl cursor-pointer py-3 px-16 ${view === "cart" ? "text-[#9538E2] bg-white font-extrabold":"bg-transparent border border-[#fff] text-white"}`}>Cart</button>
-                    <button onClick={()=> setView("wishlist")}
-                    className={`rounded-4xl py-3 cursor-pointer px-16 ${view === "wishlist"? "text-[#9538E2] bg-white font-extrabold": "bg-transparent border border-[#fff] text-white"}`}>Wishlist</button>
+                    <button onClick={() => setView("cart")} className={`rounded-4xl cursor-pointer py-3 px-16 ${view === "cart" ? "text-[#9538E2] bg-white font-extrabold" : "bg-transparent border border-[#fff] text-white"}`}>Cart</button>
+                    <button onClick={() => setView("wishlist")}
+                        className={`rounded-4xl py-3 cursor-pointer px-16 ${view === "wishlist" ? "text-[#9538E2] bg-white font-extrabold" : "bg-transparent border border-[#fff] text-white"}`}>Wishlist</button>
                 </div>
             </div>
 
@@ -58,19 +77,19 @@ const Dashboard = () => {
                 <div className="flex justify-between items-center">
                     <div><h3 className="font-bold text-2xl text-[#0B0B0B]">
                         {view === "cart" ? "Cart" : "WishList"}
-                        </h3></div>
+                    </h3></div>
                     <div className="flex items-center gap-3">
-                        <h6 className="font-bold text-2xl text-[#0B0B0B]">Total Cost: ${totalPrice(displayedList).toFixed(2)}</h6>
-                        <button className="btn btn-accent">Sort by Price</button>
-                        <button className="btn btn-info"> Purchase</button>
+                        <h6 className="font-bold text-2xl text-[#0B0B0B]">Total Cost: ${totalPrice(displayedList)}</h6>
+                        <button onClick={()=>handleSort()} className="text-[#9538E2] text-[18px] font-semibold py-3 px-6 rounded-4xl border cursor-pointer border-[#9538E2]">Sort by Price</button>
+                        <button className="font-medium text-[18px] text-white bg-[#9538E2] py-3 px-6 rounded-4xl cursor-pointer"> Purchase</button>
                     </div>
                 </div>
             </div>
 
             <div>
-                 {
-                displayedList.map( product => (<CartProduct key={product.product_id} product={product}></CartProduct>))
-                 }
+                {
+                    displayedList.map(product => (<CartProduct key={product.product_id} product={product}></CartProduct>))
+                }
 
             </div>
 
