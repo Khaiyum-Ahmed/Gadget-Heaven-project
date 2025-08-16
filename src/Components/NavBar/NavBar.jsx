f31990586617bd62d92ae3647905d9e81652ae71
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-08-16 18:25:09 +0600

    removed item function added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,12 +3,12 @@ import { FaRegHeart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 const NavBar = () => {
     const links = <>
-          
-            <li><NavLink to='/' className={({isActive, isPending})=> isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Home</NavLink></li>
-            <li><NavLink to='statistics' className={({isActive, isPending})=> isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Statistics</NavLink></li>
-            <li><NavLink to='dashboard' className={({isActive, isPending})=> isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Dashboard</NavLink></li>
-            
-            
+
+        <li><NavLink to='/' className={({ isActive, isPending }) => isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Home</NavLink></li>
+        <li><NavLink to='statistics' className={({ isActive, isPending }) => isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Statistics</NavLink></li>
+        <li><NavLink to='dashboard' className={({ isActive, isPending }) => isActive ? `text-[#9538E2] font-bold text-xl` : isPending ? `text-[rgba(11,11,11,.7)] font-medium text-base` : ''} >Dashboard</NavLink></li>
+
+
     </>
     return (
         <div className="navbar ">
@@ -20,7 +20,7 @@ const NavBar = () => {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-[rgba(11,11,11,.7)] font-medium text-base">
-                       {links}
+                        {links}
                     </ul>
                 </div>
                 <a href="/" className="btn btn-ghost text-xl text-[#0B0B0B] font-bold">Gadget Heaven</a>
@@ -31,8 +31,17 @@ const NavBar = () => {
                 </ul>
             </div>
             <div className="navbar-end gap-3">
-                <div className="btn w-[40px] h-[40px] border border-[rgba(11,11,11,.1)] rounded-full flex justify-center items-center"><span className="text-xl"><IoCartOutline /></span></div>
-                <div className="btn w-[40px] h-[40px] border border-[rgba(11,11,11,.1)] rounded-full flex justify-center items-center"><span className="text-xl"><FaRegHeart /></span></div>
+
+                <div className="indicator btn w-[40px] h-[40px] border border-[rgba(11,11,11,.1)] rounded-full flex justify-center items-center">
+                    <span className="indicator-item text-red-500">1</span>
+                    <span className="text-xl"><IoCartOutline /></span>
+                </div>
+
+                <div className="indicator btn w-[40px] h-[40px] border border-[rgba(11,11,11,.1)] rounded-full flex justify-center items-center">
+                    <span className="indicator-item text-red-500">3</span>
+                    <span className="text-xl"><FaRegHeart /></span>
+                </div>
+
             </div>
         </div>
     );
